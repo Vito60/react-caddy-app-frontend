da2d093913d89eaa365325de558ae47dca467ef5
Vito60 <randallw1996@gmail.com>
2020-05-15 12:30:26 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { Switch, Route } from 'react-router-dom'
+import Nav from './components/Nav'
+import NewPro from './components/pro/NewPro'
+import ClubForm from './components/clubs/ClubForm'
+import ProsContainer from './containers/ProsContainer'
+import Home from './components/home/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import { connect } from 'react-redux'
+import { fetchPros } from './actions/Pros.js'
+
+class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchPros()
+  }
+  
+  render() {
+
+    return (
+      <div> 
+        <Nav/>
+          <Switch> 
+            <Route exact path="/" component={Home}/> 
+            <Route exact path="/pros" component={ProsContainer}/>
+            <Route exact path="/pro/new" component={NewPro}/>
+            <Route exact path="/pro/:id/club/new" component={ClubForm}/>
+          </Switch>
+      </div>
+    );
 }
 
-export default App;
+}
+
+export default connect(null, { fetchPros })(App);
